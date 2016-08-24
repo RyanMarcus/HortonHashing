e333fa26db9909c5783c526a655f8dc4f33222b5
Ryan Marcus <ryan@rmarcus.info>
2016-08-24 02:04:23 -0400

    changed bucket from ArrayList to array

--- src/main/java/info/rmarcus/horton/HortonHashBucket.java
+++ src/main/java/info/rmarcus/horton/HortonHashBucket.java
@@ -1,36 +1,35 @@
 package info.rmarcus.horton;
 
 import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class HortonHashBucket<K, V> {
 	private boolean typeA;
-	private List<KVPair<K, V>> entries;
+	//private List<KVPair<K, V>> entries;
+	private KVPair<K, V>[] entries;
 
+	@SuppressWarnings("unchecked")
 	public HortonHashBucket(int capacity) {
-		entries = new ArrayList<>(capacity);
 		typeA = true;
-		for (int i = 0; i < capacity; i++)
-			entries.add(null);
+		entries = new KVPair[capacity];
 	}
 
-	public boolean hasEmptySlot() {
-		return entries.stream().anyMatch(kv -> kv == null);
-	}
+
 	
 	public int getNumEmptySlots() {
-		return (int) entries.stream()
-				.filter(kv -> kv == null)
-				.count();
+		int toR = 0;
+		for (int i = 0; i < entries.length; i++)
+			if (entries[i] == null) toR++;
+		
+		return toR;
 	}
 	
 	public boolean insertIfEmptyAvailable(K key, V value) {
-		for (int i = 0; i < entries.size(); i++) {
-			if (entries.get(i) == null) {
-				entries.set(i, new KVPair<K, V>(key, value));
+		for (int i = 0; i < entries.length; i++) {
+			if (entries[i] == null) {
+				entries[i] = new KVPair<K, V>(key, value);
 				return true;
 			}
 		}
@@ -43,31 +42,31 @@ public class HortonHashBucket<K, V> {
 	}
 	
 	public KVPair<K, V> getKVPair(K key) {
-		for (int i = (typeA ? 0 : 1); i < entries.size(); i++) {
-			if (entries.get(i) == null)
+		for (int i = (typeA ? 0 : 1); i < entries.length; i++) {
+			if (entries[i] == null)
 				continue; 
 			
-			if (entries.get(i).key.equals(key))
-				return entries.get(i);
+			if (entries[i].key.equals(key))
+				return entries[i];
 		}
 		
 		return null;
 	}
 	
 	public KVPair<K, V> convertToTypeB() {
-		KVPair<K, V> toR = entries.get(0);
-		entries.set(0, new RedirectList<K, V>());
+		KVPair<K, V> toR = entries[0];
+		entries[0] = new RedirectList<K, V>();
 		typeA = false;
 		return toR;
 	}
 	
 	public void revertToTypeA(KVPair<K, V> toAddBack) {
-		entries.set(0, toAddBack);
+		entries[0] = toAddBack;
 		typeA = true;
 	}
 
 	public KVPair<K, V> getKVPair(int idx) {
-		return (typeA ? entries.get(idx) : entries.get(idx + 1));
+		return (typeA ? entries[idx] : entries[idx + 1]);
 	}
 	
 	public K getKey(int idx) {
@@ -75,7 +74,7 @@ public class HortonHashBucket<K, V> {
 	}
 	
 	public int getCapacity() {
-		return (typeA ? entries.size() : entries.size() - 1);
+		return (typeA ? entries.length : entries.length - 1);
 	}
 
 	public boolean isTypeA() {
@@ -86,19 +85,19 @@ public class HortonHashBucket<K, V> {
 		if (typeA)
 			throw new HortonHashMapRuntimeException("Trying to set a redirect table entry on a type A bucket");
 		
-		((RedirectList<K, V>) entries.get(0)).redirectList[idx] = value;
+		((RedirectList<K, V>) entries[0]).redirectList[idx] = value;
 	}
 	
 	public short getRedirectListEntry(int idx) {
 		if (typeA)
 			throw new HortonHashMapRuntimeException("Trying to set a redirect table entry on a type A bucket");
 	
-		return ((RedirectList<K, V>) entries.get(0)).redirectList[idx];
+		return ((RedirectList<K, V>) entries[0]).redirectList[idx];
 
 	}
 
 	public Set<java.util.Map.Entry<K, V>> entrySet() {
-		return entries.stream()
+		return Arrays.stream(entries)
 				.filter(kv -> kv != null)
 				.filter(kv -> kv.key != null)
 				.map(kv -> new SimpleEntry<K, V>(kv.key, kv.value))
@@ -106,7 +105,7 @@ public class HortonHashBucket<K, V> {
 	}
 
 	public void clearKVPair(int itemIdx) {
-		entries.set(itemIdx, null);
+		entries[itemIdx] = null;
 	}
 	
 
